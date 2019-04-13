9a924ce915466ce4b62e850a21021a6dea975948
Juan Camilo Sánchez Urrego <jcsu2019369@gmail.com>
2019-04-13 10:40:05 -0500

    Added Project Files

---
+++ app.js
@@ -0,0 +1,19 @@
+var https = require('https');
+var express = require('express');
+var subdomain = require('express-subdomain');
+var app = express();
+var router = express.Router();
+var fs = require('fs');
+
+app.use(subdomain('jcsu',router));
+
+var server = https.createServer({
+    key: fs.readFileSync('/mnt/DATOS Linux/Dropbox/Documentos/2019/Projects 2019/Certificado HTTPS/clave_priv.key'),
+    cert: fs.readFileSync('/mnt/DATOS Linux/Dropbox/Documentos/2019/Projects 2019/Certificado HTTPS/certificado.crt'),
+},app).listen(443,()=>{
+    console.info("Servidor Seguro Escuchando ...");
+});
+
+router.get('/',(req,res)=>{
+    res.send("Conexion Exitosa");
+});
